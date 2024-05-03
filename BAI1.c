e42a2b321e862196c07a90e289109442a1c527c5
tamnguyen2203 <nbtam2202@gmail.com>
2024-05-03 14:10:50 +0700

    Create BAI1.c

---
+++ BAI1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int i;
+    
+    printf("Cac so nguyen co 2 chu so va la boi cua 7:\n");
+    
+    for (i = 14; i < 100; i += 7) {
+        printf("%d ", i);
+    }
+    
+    return 0;
+}
